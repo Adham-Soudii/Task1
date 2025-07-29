ef8b93aa293b402743d8170121d3dfa8cb54ba27
Adham-Soudi <adhamsoudi23@gmail.com>
2025-07-30 02:55:54 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("Carpet Cleaning Service\n");
+
+int LargeCarpetCost = 35;
+int SmallCarpetCost = 25;
+double serviceRate = 0.06 ;
+Console.Write("Enter number of large carpet: ");
+int largecount = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number of small carpet: ");
+int smallcount = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("35 for Large One \n" + "25 for Small One \n");
+
+int total_BS = (largecount * LargeCarpetCost) + (smallcount * SmallCarpetCost);
+double service6 = total_BS * 0.06;
+double total_FS = total_BS + service6;
+Console.WriteLine($"total: {total_BS}");
+Console.WriteLine($"Service (6%): {serviceRate}");
+Console.WriteLine($"Summary: {total_FS}\n");
+Console.WriteLine("Estimates Are Valid For 30 Days\n ");
+
+
+
+
