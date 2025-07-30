a0c579d2c5081d1c5bc8efdcf3894d6d4906f880
Adham-Soudi <adhamsoudi23@gmail.com>
2025-07-30 12:03:17 +0300

    Add Dollar ($)

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -11,14 +11,14 @@ Console.Write("Enter number of small carpet: ");
 int smallcount = int.Parse(Console.ReadLine());
 Console.WriteLine();
 
-Console.WriteLine("35 for Large One \n" + "25 for Small One \n");
+Console.WriteLine("$35 for Large One \n" + "$25 for Small One \n");
 
 int total_BS = (largecount * LargeCarpetCost) + (smallcount * SmallCarpetCost);
 double service6 = total_BS * 0.06;
 double total_FS = total_BS + service6;
-Console.WriteLine($"total: {total_BS}");
+Console.WriteLine($"total: ${total_BS}");
 Console.WriteLine($"Service (6%): {serviceRate}");
-Console.WriteLine($"Summary: {total_FS}\n");
+Console.WriteLine($"Summary: ${total_FS}\n");
 Console.WriteLine("Estimates Are Valid For 30 Days\n ");
 
 
