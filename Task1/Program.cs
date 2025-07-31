74af74db21f2262cb5c9bb91962760a2a97190f8
Adham-Soudi <adhamsoudi23@gmail.com>
2025-07-31 19:57:56 +0300

    Using Convert Class

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,10 +5,10 @@ int LargeCarpetCost = 35;
 int SmallCarpetCost = 25;
 double serviceRate = 0.06 ;
 Console.Write("Enter number of large carpet: ");
-int largecount = int.Parse(Console.ReadLine());
+int largecount = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Enter number of small carpet: ");
-int smallcount = int.Parse(Console.ReadLine());
+int smallcount = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
 
 Console.WriteLine("$35 for Large One \n" + "$25 for Small One \n");
@@ -17,7 +17,7 @@ int total_BS = (largecount * LargeCarpetCost) + (smallcount * SmallCarpetCost);
 double service6 = total_BS * 0.06;
 double total_FS = total_BS + service6;
 Console.WriteLine($"total: ${total_BS}");
-Console.WriteLine($"Service (6%): {serviceRate}");
+Console.WriteLine($"Tax (6%): {serviceRate}");
 Console.WriteLine($"Summary: ${total_FS}\n");
 Console.WriteLine("Estimates Are Valid For 30 Days\n ");
 
